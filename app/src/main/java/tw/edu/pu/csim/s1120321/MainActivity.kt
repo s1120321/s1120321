9c96217482b51c03278bccbcc355831f3dbfb5ab
s1120321 <yieewenhong@gmail.com>
2024-12-09 14:42:27 +0800

    v1_洪藝文

--- app/src/main/java/tw/edu/pu/csim/s1120321/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1120321/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.s1120321.ui.theme.S1120321Theme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
+
                     )
                 }
             }
